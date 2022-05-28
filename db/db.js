62e2e2d3400609145599e2995845175c4dcb9060
abusalem mangalwedhe <abusalemmangalwedhe@gmail.com>
2022-05-28 20:59:37 +0530

    Portfolio Done!

---
+++ db/db.js
@@ -0,0 +1,214 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Abusalem Mangalwedhe. I’m currently working as a Software Engineer at Biofourmis India specialized in JavaScript Applications. I did my undergrad in CS from NIT Warangal.",
+				"I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+				"I love to read, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: vinaysomawat40@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "JAVA, JavaScript",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "SpringBoot, JDBC, Hibernate",
+			color: "6",
+			percentage: "80",
+		},
+		{
+			title: "Backend",
+			skillName: "Mysql, PostgreSQL",
+			color: "2",
+			percentage: "65",
+		},
+		{
+			title: "Clouds",
+			skillName: "AWS, Docker, Netlify",
+			color: "3",
+			percentage: "70",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS",
+			color: "4",
+			percentage: "55",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub, JIRA, Trello",
+			color: "7",
+			percentage: "70",
+		},
+		{
+			title: "Tools",
+			skillName: "Postman, Chrome DevTools",
+			color: "3",
+			percentage: "80",
+		},
+		{
+			title: "Saas products",
+			skillName: "CleverTap, FreshDesk",
+			color: "5",
+			percentage: "50",
+		},
+		{
+			title: "Editor",
+			skillName: "Intellij, VS Code, SpringSuiteTool",
+			color: "6",
+			percentage: "70",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "Fitmeals Clone",
+				image: "https://www.fitmeals.co.in/wp-content/uploads/2019/06/logo-black.png",
+				summary:
+					"Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+				preview: "https://chic-crumble-988843.netlify.app/",
+				techStack: [
+					"JavaScript",
+					"HTML",
+					"CSS",
+					"Netlify",
+				],
+			},
+			{
+				projectName: "Pepperfry Clone",
+				image: "https://ii1.pepperfry.com/images/svg/pf-logo-21.svg",
+				summary:
+					"Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+				preview: "https://clever-fermi-0d5d76.netlify.app",
+				techStack: ["JavaScript",
+				"HTML",
+				"CSS",
+				"Netlify",
+			],
+			},
+		],
+	},
+	education: [
+		{
+			title: "Java Backend Development",
+			duration: "   2021 - Present",
+			subtitle: "Masai School Bangalore",
+			details: [
+				"Hands on Experience on Industry level Projects.",
+				"Active Competitive Programmer with Hakerrank Problem Solving Badges with 5 STAR.",
+				"10000+ Lines of code.",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Java",
+				"Core-Java",
+				"Database Management System",
+				"Spring, SpringBoot",
+				"System Design",
+				"Hibernate, JDBC",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Diploma in Computer Science and Engineering",
+			duration: "   2018 - 2021",
+			subtitle: "Bharati Vidyapeeth's Institute of Technology(Polytechnic), Palus",
+			details: [
+				"Online Attendance Management System Using Face Recognition.",
+				"Secured 1st Rank in state-level Project Compitation.",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Object Oriented Programming",
+				"Operating Systems",
+				"Database Management System",
+				"Computer Networks",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "10TH(SCC)",
+			duration: "2017 - 2018",
+			subtitle: "Shri Guru Basveshwar High School Billur.",
+			details: [
+				"76.40%",
+			],
+			tags: [
+				"",
+				
+			],
+			icon: "graduation-cap",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "Stackoverflow",
+					link: "https://stackoverflow.com/users/15893142/abusalem-mangalwedhe",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/Abusalem9",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/Abusalem/",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				{
+					text: "Clone this page",
+					link: "https://github.com/vinaysomawat/abusalem.github.io",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/abusalem-mangalwedhe-628901180/",
+				},
+				{
+					text: "Instagram",
+					link: "https://www.instagram.com/abusalem.ershad/",
+				},
+				{
+					text: "Buy me a coffee",
+					link: "https://www.buymeacoffee.com/r194dME8y",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Abusalem Mangalwedhe.",
+				"&copy; No Copyrights."
+			],
+		},
+	],
+};
